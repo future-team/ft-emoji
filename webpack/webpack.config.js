48d79b6241cec422a05b42bbe57e112802996bcb
amberonno <amberonno@gmail.com>
2017-08-25 14:46:16 +0800

    调小图片大小

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -87,7 +87,7 @@ module.exports = {
             },
             {
                 test: /\.(png|jpg|gif)$/,
-                loader: 'url?limit=50000'
+                loader: 'url?limit=5000'
             }
             /*,
                          {
